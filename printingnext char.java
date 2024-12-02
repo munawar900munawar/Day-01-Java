02d277908031d86af254ce4af678503c2af06b19
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-02 15:30:42 +0530

    Add files via upload

---
+++ printingnext char.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class printingnext{
+    public static void main(){
+        char c='a';
+        int b='c';
+        b+=1;
+        char v=(char)b;
+        system.out.println(v);
+    }
+}
